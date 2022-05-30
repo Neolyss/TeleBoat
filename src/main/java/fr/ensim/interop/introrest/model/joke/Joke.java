b2699af2b932a80c4b49b1c151ca39468f2407a8
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2022-05-30 20:04:33 +0200

    Debut de la mise en place de la BDD H2 pour les notes des jokes

--- src/main/java/fr/ensim/interop/introrest/model/joke/Joke.java
+++ src/main/java/fr/ensim/interop/introrest/model/joke/Joke.java
@@ -1,8 +1,16 @@
 package fr.ensim.interop.introrest.model.joke;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Joke{
-    public int id;
+
+    @Id
+    public Long id;
     public String type;
     public String joke;
     public String answer;
+    public int note;
+
 }
