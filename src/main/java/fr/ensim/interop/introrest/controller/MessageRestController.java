753200fc68224a10d8950e684041eb3729f574af
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2022-05-30 19:36:21 +0200

    La récuparation d'une joke fonctionne

--- src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -1,7 +1,14 @@
 package fr.ensim.interop.introrest.controller;
 
+import fr.ensim.interop.introrest.model.joke.Joke;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 @RestController
 public class MessageRestController {
@@ -10,4 +17,21 @@ public class MessageRestController {
 	private String telegramApiUrl;
 	
 	//Opérations sur la ressource Message
+}
+
+class JokeCall {
+	@Value("${blagues.api.url}")
+	private static String blaguesApiUrl;
+	@Value("${blagues.api.token}")
+	private static String blaguesApiToken;
+
+	public static Joke getJoke () throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(blaguesApiToken);
+		ResponseEntity<Joke> responseJoke = restTemplate.exchange(RequestEntity.get(
+				new URI(blaguesApiUrl + "api/random")).headers(headers).build(), Joke.class);
+		if (responseJoke.getStatusCode() != HttpStatus.OK) throw new Exception("Code "+responseJoke.getStatusCode().value());
+		return responseJoke.getBody();
+	}
 }
---
+++ src/main/java/fr/ensim/interop/introrest/model/joke/Joke.java
@@ -0,0 +1,8 @@
+package fr.ensim.interop.introrest.model.joke;
+
+public class Joke{
+    public int id;
+    public String type;
+    public String joke;
+    public String answer;
+}
