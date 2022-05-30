173cfec86065b81fe77e26d86556b643b7a67f45
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2022-05-30 17:08:14 +0200

    Fonction getWeather terminée

--- src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -1,7 +1,10 @@
 package fr.ensim.interop.introrest.controller;
 
+import fr.ensim.interop.introrest.model.openWeather.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MessageRestController {
@@ -10,4 +13,42 @@ public class MessageRestController {
 	private String telegramApiUrl;
 	
 	//Opérations sur la ressource Message
+}
+
+class OpenWeatherCall {
+	@Value("${open.weather.api.url}")
+	private static String openWeatherUrl;
+	@Value("${open.weather.api.token}")
+	private static String openWeatherToken;
+
+	/**
+	 * Recherche de la météo sur une ville
+	 * @param cityName : nom de la ville
+	 * @return Objet OpenWeather qui contient la meteo
+	 * @throws Exception
+	 */
+	public static OpenWeather getWeather (String cityName, TimeOfWeek moment) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<City[]> responseEntity = restTemplate.getForEntity(openWeatherUrl + "geo/1.0/direct?q={cityName}&limit=3"
+						+ "&appid=" + openWeatherToken,
+				City[].class, cityName);
+
+		City[] cities = responseEntity.getBody();
+		if (cities.length == 0) throw new Exception("Aucune ville trouvé");
+		City city = cities[0];
+
+		OpenWeather openWeather;
+
+		if(moment == TimeOfWeek.TODAY)
+			openWeather = restTemplate.getForObject(openWeatherUrl + "data/2.5/weather?lat="+city.getLat()
+							+ "&lon="+city.getLat()+"&appid=" + openWeatherToken,
+					OpenWeatherCurrent.class);
+		else if (moment == TimeOfWeek.WEEK)
+			openWeather = restTemplate.getForObject(openWeatherUrl + "data/2.5/onecall?lat=" + city.getLat()
+							+ "&lon=" + city.getLat() + "&&exclude=current,minutely,hourly&appid=" + openWeatherToken,
+					OpenWeatherForcast.class);
+		else throw new Exception("TimeOfWeek non reconu");
+
+		return openWeather;
+	}
 }
---
+++ src/main/java/fr/ensim/interop/introrest/model/openWeather/OpenWeatherForcast.java
@@ -0,0 +1,95 @@
+package fr.ensim.interop.introrest.model.openWeather;
+
+import java.util.ArrayList;
+
+
+public class OpenWeatherForcast implements OpenWeather {
+    private float lat;
+    private float lon;
+    private String timezone;
+    private float timezone_offset;
+    ArrayList<Object> daily = new ArrayList<Object>();
+
+
+    // Getter Methods
+
+    public float getLat() {
+        return lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public float getTimezone_offset() {
+        return timezone_offset;
+    }
+
+    // Setter Methods
+
+    public void setLat( float lat ) {
+        this.lat = lat;
+    }
+
+    public void setLon( float lon ) {
+        this.lon = lon;
+    }
+
+    public void setTimezone( String timezone ) {
+        this.timezone = timezone;
+    }
+
+    public void setTimezone_offset( float timezone_offset ) {
+        this.timezone_offset = timezone_offset;
+    }
+}
+
+class Daily{
+    public int dt;
+    public int sunrise;
+    public int sunset;
+    public int moonrise;
+    public int moonset;
+    public double moon_phase;
+    public Temp temp;
+    public FeelsLike feels_like;
+    public int pressure;
+    public int humidity;
+    public double dew_point;
+    public double wind_speed;
+    public int wind_deg;
+    public double wind_gust;
+    public ArrayList<Weather> weather;
+    public int clouds;
+    public double pop;
+    public double uvi;
+    public double rain;
+}
+
+class FeelsLike{
+    public double day;
+    public double night;
+    public double eve;
+    public double morn;
+}
+
+class Temp{
+    public double day;
+    public double min;
+    public double max;
+    public double night;
+    public double eve;
+    public double morn;
+}
+
+//public class Weather{
+//    public int id;
+//    public String main;
+//    public String description;
+//    public String icon;
+//}
+
