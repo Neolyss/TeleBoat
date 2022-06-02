4c55ad4da4d12a6236a6163ca27af50167dfd0c4
Jessy LEBRUN <Jessy.Lebrun.Etu@univ-lemans.fr>
2022-06-02 14:27:02 +0200

    Ajout de corectif

--- src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -84,9 +84,9 @@ public class MessageRestController {
 	}
 
 	@PostMapping("/joke")
-	public String postNoteJoke (@RequestParam(name = "note") int noteGET) {
+	public String postNoteJoke (@RequestParam(name = "note") int notePOST) {
 		Joke joke = new Joke();
-		return JokeCall.addNote(notesDAO, joke, noteGET) ? "Votre note a été ajouté" : "J'ai pas pu ajouté votre note";
+		return JokeCall.addNote(notesDAO, joke, notePOST) ? "Votre note a été ajouté" : "J'ai pas pu ajouté votre note";
 	}
 }
 
